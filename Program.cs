5c185e970b5eb890e68dd8c90dc963455b230de3
Sebastian-Devus <86898507+SebastianDevus@users.noreply.github.com>
2024-03-13 18:56:05 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int alimentos;
+int distribuidos = 0;
+int loops = 1;
+
+Console.WriteLine("----- Esperto contra sabido -----\n");
+Console.Write("Quantos alimentos serão distribuídos? ");
+alimentos = Convert.ToInt32(Console.ReadLine());
+
+while (alimentos > distribuidos)
+{
+    Console.Write($"{loops} para você, ");
+    distribuidos++;
+
+    Console.Write($"1");
+    distribuidos++;
+
+    if (loops > 1)
+    {
+        for (int i = 2; i <= loops && distribuidos < alimentos; i++)
+        {
+            Console.Write($", {i}");
+            distribuidos++;
+        }
+    }
+
+    Console.WriteLine(" para mim.");
+
+    loops++;
+}
